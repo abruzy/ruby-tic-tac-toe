b99790302da3c04dac91eb8b7c4d69a14e6c7f4d
Biodun <biodun9@gmail.com>
2019-10-25 20:23:30 +0100

    added players picking their preferred letter for the game

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,10 @@
 puts 'You are welcome to this TIC-TAC-TOE game'
 puts 'Please state your name'
 Player1 = gets.chomp.capitalize 
+puts "#{Player1},PLease pick your preferred letter,X or O"
+tool1 = gets.chomp.upcase
+tool2 = tool1 == 'X' ? 'O' : 'X'
+puts 'Second player,please state your name'
+Player2 = gets.chomp.capitalize
+puts "The first player chose '#{tool1}',so you are stuck with '#{tool2}' in this round"
+puts ''
