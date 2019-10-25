e329f84f205fe662109b1b563269737e4828b818
Biodun <biodun9@gmail.com>
2019-10-25 20:34:41 +0100

    added edge case for number greater than 9 and less than 1

--- bin/main.rb
+++ bin/main.rb
@@ -11,3 +11,8 @@ puts "The first player chose '#{tool1}',so you are stuck with '#{tool2}' for thi
 puts ''
 puts "Now that's out of the way,let the game begin"
 puts ''
+position=10
+while position<1 || position>9
+puts "#{Player1}, What position would you like to make your move?, PLease pick a number between 0 and 10"
+position1 = gets.chomp
+end
