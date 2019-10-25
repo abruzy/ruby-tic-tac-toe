4650cd4fe198b380a27546349069e2892bad081d
Biodun <biodun9@gmail.com>
2019-10-25 20:33:29 +0100

    added second player introduction and letter for game

--- bin/main.rb
+++ bin/main.rb
@@ -6,3 +6,8 @@ puts "#{Player1},PLease pick your preferred letter,X or O"
 tool1 = gets.chomp.upcase
 tool2 = tool1 == 'X' ? 'O' : 'X'
 puts 'Second player,please state your name'
+Player2 = gets.chomp.capitalize
+puts "The first player chose '#{tool1}',so you are stuck with '#{tool2}' for this round"
+puts ''
+puts "Now that's out of the way,let the game begin"
+puts ''
