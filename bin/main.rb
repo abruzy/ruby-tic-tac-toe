54f58b559427f68fa7108067fb59c5a7c6358c00
Biodun <biodun9@gmail.com>
2019-10-25 20:27:49 +0100

    added position establishment for PLayer1

--- bin/main.rb
+++ bin/main.rb
@@ -7,5 +7,12 @@ tool1 = gets.chomp.upcase
 tool2 = tool1 == 'X' ? 'O' : 'X'
 puts 'Second player,please state your name'
 Player2 = gets.chomp.capitalize
-puts "The first player chose '#{tool1}',so you are stuck with '#{tool2}' in this round"
+puts "The first player chose '#{tool1}',so you are stuck with '#{tool2}' for this round"
 puts ''
+puts "Now that's out of the way,let the game begin"
+puts ''
+position=10
+while position<0 || position>9
+puts "#{Player1}, What position would you like to make your move?, PLease pick a number between 0 and 10"
+position1 = gets.chomp
+end
