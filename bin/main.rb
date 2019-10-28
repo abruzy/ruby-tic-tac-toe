e3a2c505cce8c12bb6f650c127e6785eef5a187f
Biodun <biodun9@gmail.com>
2019-10-28 18:03:02 +0100

    added edge case to make sure players pick different names

--- bin/main.rb
+++ bin/main.rb
@@ -14,9 +14,11 @@ player1 = Player.new(name, tool)
 
 puts "Next player, please enter your name"
 name2 = gets.chomp.capitalize
-
+until name2!=name
+    puts "Please pick another name,that name is taken"
+    name2 = gets.chomp.upcase
+end
 
 tool2 = tool=='X'? "O" : "X"
 
 player2 = Player.new(name2, tool2)
-puts player2.tool
