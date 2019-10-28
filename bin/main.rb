6364a18a25ba51c53f2ae1f35df0a395cc921503
abruzy <dialloabubakar5861@gmail.com>
2019-10-28 17:27:45 +0100

    added user interface for new player

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,12 @@
 require_relative '../lib/board.rb'
+
+puts "Enter your name"
+name = gets.chomp
+
+puts "What tools would you like to choose"
+tool = gets.chomp
+
+player1 = Player.new(name, tool)
+puts player1.name
+
+puts player1.tool
