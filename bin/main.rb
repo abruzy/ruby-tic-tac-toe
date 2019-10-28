c57ac2c8bc6a67fa088820025368479e64f0304f
Biodun <biodun9@gmail.com>
2019-10-28 21:57:31 +0100

    added main.rb to merge

--- bin/main.rb
+++ bin/main.rb
@@ -25,21 +25,7 @@ puts ''
 arr=[]
 position1 = 10
 game_on=true
-while game_on
-    puts "#{Player1}, What position would you like to make your move?, PLease pick a number between 0 and 10"
-    position1 = gets.chomp.to_i
-    until !arr.include? position1
-      puts "That position is occupied,pls re-pick?"
-      position1 = gets.chomp.to_i
-    end
-    arr << position1
-      puts "Now your move with '#{tool1}' is displayed on the board"
-    puts "#{Player2}, What position would you like to make your move?, PLease pick a number between 0 and 10"
-    position2 = gets.chomp.to_i
-    until !arr.include? position2
-      puts "That position is occupied,pls re-pick?"
-      position2 = gets.chomp.to_i
-    end
+
       
       arr << position2
       puts "Now your move with '#{tool2}' is displayed on the board"
