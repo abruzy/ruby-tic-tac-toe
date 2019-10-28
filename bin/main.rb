6eeca8c7afeeb767b739c3573af8c2c65cead3a9
Biodun <biodun9@gmail.com>
2019-10-28 17:55:31 +0100

    ADDED USER INTERFACE FOR PLAYER2

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,18 @@
 require_relative '../lib/board.rb'
 
 puts "Enter your name"
-name = gets.chomp
+name = gets.chomp.capitalize
 
 puts "What tools would you like to choose"
-tool = gets.chomp
+tool = gets.chomp.upcase
 
 player1 = Player.new(name, tool)
-puts player1.name
 
-puts player1.tool
+puts "Next player, please enter your name"
+name2 = gets.chomp.capitalize
+
+
+tool2 = tool=='X'? "O" : "X"
+
+player2 = Player.new(name2, tool2)
+puts player2.tool
