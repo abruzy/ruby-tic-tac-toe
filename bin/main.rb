4581aa1d0e365ed53aef63170e3b3914ca399ddb
Biodun <biodun9@gmail.com>
2019-10-29 01:09:21 +0100

    added user module and initialized game class

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/board.rb'
 
+module User
+    def self.display(arr)
+        line_width = 50
+    puts "#{arr[1]} | #{arr[2]} | #{arr[3]} ".center line_width
+    puts "#{arr[4]} | #{arr[5]} | #{arr[6]} ".center line_width
+    puts "#{arr[7]} | #{arr[8]} | #{arr[9]} ".center line_width
+    end
+end
 puts "Enter your name"
 name = gets.chomp.capitalize
 
@@ -22,15 +30,17 @@ end
 tool2 = tool=='X'? "O" : "X"
 
 player2 = Player.new(name2, tool2)
+played=Game.new
 i=0
 while i<9
     i+=1
 puts "#{player1.name}, please pick a position for your move"
 move1=gets.chomp.to_i
-Game.play(move1,tool)
-Game.display
+played.play(move1,tool)
+User.display(played.board)
 puts "#{player2.name}, please pick a position for your move"
 move2=gets.chomp.to_i
-Game.play(move2,tool2)
-Game.display
+played.play(move2,tool2)
+User.display(played.board)
+
 end
