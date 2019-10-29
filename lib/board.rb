eb322edfff9db63bd2f6f446bfb7baf2efea1c4c
Biodun <biodun9@gmail.com>
2019-10-29 21:23:41 +0100

    made development branch mergeable

--- lib/board.rb
+++ lib/board.rb
@@ -11,20 +11,4 @@ class Player
 end
 
 class Game
-  @board = [' 1', ' 2', ' 3', ' ', ' ', ' ', ' ', ' ', ' ']
-
-  def self.display
-    line_width = 50
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " .center line_width
-    puts '-------------' .center line_width
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} ".center line_width
-    puts '-------------' .center line_width
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} ".center line_width
-  end
-
-  def self.play(position, tool)
-    @position = position
-    @tool = tool
-    @board[@position - 1] = @tool
-  end
 end
