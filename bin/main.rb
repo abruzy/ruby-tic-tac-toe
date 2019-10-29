0ba68c7fabb88482d4fa7931add9fbda51723391
Biodun <biodun9@gmail.com>
2019-10-29 16:30:30 +0100

    added check method for umpire module

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/board.rb'
 
-module User
+module Umpire
     def self.display(arr)
         line_width = 50
      puts "#{arr[1]} | #{arr[2]} | #{arr[3]} ".center line_width
@@ -8,13 +8,13 @@ module User
      puts "#{arr[7]} | #{arr[8]} | #{arr[9]} ".center line_width
     end
 
-    def check
-        return $arr[1] if $arr[1]==$arr[2] && $arr[1] ==$arr[3]
-        return $arr[4] if $arr[4]==$arr[5] && $arr[4] ==$arr[6]
-        return $arr[7] if $arr[7]==$arr[8] && $arr[7] ==$arr[9]
-        return $arr[1] if $arr[1]==$arr[5] && $arr[1] ==$arr[9]
-        return $arr[3] if $arr[3]==$arr[5] && $arr[7] ==$arr[3]
-        return $arr[1] if $arr[1]==$arr[4] && $arr[1] ==$arr[7]
+    def self.check(arr)
+        return arr[1] if arr[1]==arr[2] && arr[1] ==arr[3]
+        return arr[4] if arr[4]==arr[5] && arr[4] ==arr[6]
+        return arr[7] if arr[7]==arr[8] && arr[7] ==arr[9]
+        return arr[1] if arr[1]==arr[5] && arr[1] ==arr[9]
+        return arr[3] if arr[3]==arr[5] && arr[7] ==arr[3]
+        return arr[1] if arr[1]==arr[4] && arr[1] ==arr[7]
     
     end
 end
@@ -40,17 +40,26 @@ end
 tool2 = tool=='X'? "O" : "X"
 
 player2 = Player.new(name2, tool2)
-played=Game.new
+player_turn=Game.new
 i=0
 while i<9
     i+=1
 puts "#{player1.name}, please pick a position for your move"
 move1=gets.chomp.to_i
-played.play(move1,tool)
-User.display(played.board)
+player_turn.play(move1,tool)
+Umpire.display(player_turn.board)
+if Umpire.check(player_turn.board)=='X' || Umpire.check(player_turn.board)=="O" 
+  puts "#{player1.name} is the winner of this round"
+  break
+end
+break if i==9
 puts "#{player2.name}, please pick a position for your move"
 move2=gets.chomp.to_i
-played.play(move2,tool2)
-User.display(played.board)
+player_turn.play(move2,tool2)
+Umpire.display(player_turn.board)
+if Umpire.check(player_turn.board)=='X' || Umpire.check(player_turn.board)=="O" 
+  puts "#{player2.name} is the winner of this round"
+  break
+end
 
 end
